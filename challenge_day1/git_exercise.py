5815ff08dfaf349bdd9e82831da3a72a49f6ca7c
git12hub34 <alpinista_najblizszy_7p@icloud.com>
2026-08-31 11:27:33 +0200

    ..

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,6 +2,7 @@ import aleks
 import muni
 import lula
 import ebony
+import tiara
 
 def introduction():
     print("This is Team YourTeamNameHere. We are:")
@@ -9,6 +10,7 @@ def introduction():
     print(muni.get_muni_name())
     print(lula.get_lula_name())
     print(ebony.get_ebony_name())
+    print(ebony.get_tiara_name())
 
 
 introduction()
---
+++ challenge_day1/tiara.py
@@ -0,0 +1,3 @@
+def get_tiara_name():
+    return "Tiara"
+    
