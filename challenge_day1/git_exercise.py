8c0a24584706160a4f928cfc504559f29a4740db
git12hub34 <alpinista_najblizszy_7p@icloud.com>
2026-08-31 11:17:44 +0200

    ..

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,7 +1,10 @@
 import aleks
+import muni
 
 def introduction():
     print("This is Team YourTeamNameHere. We are:")
     print(aleks.get_aleks_name())
+    print(muni.get_muni_name())
+
 
 introduction()
