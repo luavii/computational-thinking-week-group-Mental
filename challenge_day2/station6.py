2e526e09ad5d7c2a0b5082495ebda2866e40c728
Muniuva <muni.omar@student.uva.nl>
2026-09-01 14:57:48 +0200

    Clean up station6.py by removing test code
    Removed test input and output print statements from the solution function.

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -4,7 +4,3 @@ def solution_station_6(station6):
   radians = np.deg2rad(station6) 
   result = np.sin(radians)
   return round(result, 4)
-
-given_input = 73
-output = solution_station_6(given_input)
-print(output)
