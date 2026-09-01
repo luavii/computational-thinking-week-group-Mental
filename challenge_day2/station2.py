38b85f5aeba6d678816e94540960bad1fe85bb63
nandinimathhere <nandini.mathur@student.uva.nl>
2026-09-01 15:45:53 +0200

    Tweak in Code

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,15 +1,8 @@
 from datetime import datetime 
 
-def solution_station_2():
-    def date():
-        while True:
-            dateinput = input("Enter a Date (YYYY-MM-DD): ")
-            try: 
-                valid = datetime.strptime(dateinput, "%Y-%m-%d")
-                return valid 
-            except ValueError:
-                print("This is not a valid date, try again")
-    userdate = date()
+def solution_station_2(x):
+    
+    userdate  =datetime.strptime(x, "%Y-%m-%d")
     userday = userdate.strftime("%A")
     japanese_days = {
         "Monday" : "月曜日",
@@ -22,7 +15,6 @@ def solution_station_2():
         }
     return japanese_days[userday]
 
-solution_station_2()
    
 
 
