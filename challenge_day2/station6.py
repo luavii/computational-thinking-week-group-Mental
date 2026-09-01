3cc4be9eb8a251a7fa8b6e05b20d6e768ac0cf73
ebonyeah <ebony.akers-hughes@student.uva.nl>
2026-09-01 16:43:29 +0200

    no 4

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -3,5 +3,5 @@ import numpy as np
 def solution_station_6(station6):
   radians = np.deg2rad(station6) 
   result = np.sin(radians)
-  return round(result, 4)
+  return round(result)
 #might not need the 4
