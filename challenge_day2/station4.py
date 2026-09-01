946fd1f7ac4db1a29650dc27caa156ba26b2bc2c
ebonyeah <ebony.akers-hughes@student.uva.nl>
2026-09-01 16:32:19 +0200

    eee

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,12 +1,12 @@
 def solution_station_4(x):
     if x <= 1:
-        tf = False
-    else:
         tf = True
+    else:
+        tf = False
 
-        for i in range(2, int(n**0.5) + 1):
+        for i in range(2, int(x**0.5) + 1):
             if x % i == 0:
-                tf = False
+                tf = True
                 break
 
     return tf
