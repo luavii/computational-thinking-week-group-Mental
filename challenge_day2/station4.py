0a5fb33f446a94510766bdbb6a374c3e9f826d3b
ebonyeah <ebony.akers-hughes@student.uva.nl>
2026-09-01 16:37:06 +0200

    bool swithced

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,12 +1,12 @@
 def solution_station_4(x):
     if x <= 1:
-        tf = True
-    else:
         tf = False
+    else:
+        tf = True
 
         for i in range(2, int(x**0.5) + 1):
             if x % i == 0:
-                tf = True
+                tf = False
                 break
 
     return tf
