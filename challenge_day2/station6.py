3e92bab7cdd5b59a1f71f06c2e82e02720ed7b26
luavii <lulafabienne@gmail.com>
2026-09-01 17:04:11 +0200

    llll

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,10 +1,5 @@
 import numpy as np
 
 def solution_station_6(station6):
-
-  radians = np.deg2rad(station6) 
-  result = np.sin(radians)
-  results = np.deg2rad(result)
-
-  return float(results)
-#might not need the 4
+  result = np.sin(station6)
+  return float(result)
