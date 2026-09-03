fece1657273b731790940257b346675ed31b5218
Ttev22 <t.tev@hotmail.com>
2026-09-03 14:36:17 +0200

    updated

---
+++ challenge_day4/Rust_step5/target/debug/deps/evaluation-656c208cb2341ae4.d
@@ -0,0 +1,5 @@
+/Users/t.tev/computational-thinking-week-group-Mental/challenge_day4/Rust_step5/target/debug/deps/evaluation-656c208cb2341ae4.d: src/evaluation.rs
+
+/Users/t.tev/computational-thinking-week-group-Mental/challenge_day4/Rust_step5/target/debug/deps/evaluation-656c208cb2341ae4: src/evaluation.rs
+
+src/evaluation.rs:
